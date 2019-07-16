a27244276dd792ba7a0b6197829409c4ac9c104d
sflanary97 <sflanary97@gmail.com>
2019-07-15 19:02:42 -0500

    First Production
    built and optimized site

---
+++ build/precache-manifest.b929bc1175d0c6daa873d5501d640c61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc2ae677b31d947d9660bf657c9fd50b",
+    "url": "/websitetutorial/index.html"
+  },
+  {
+    "revision": "15b6edc2286d6da96ef9",
+    "url": "/websitetutorial/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "39b14b62e60ebebf9de7",
+    "url": "/websitetutorial/static/js/2.64edd0d7.chunk.js"
+  },
+  {
+    "revision": "15b6edc2286d6da96ef9",
+    "url": "/websitetutorial/static/js/main.d39d16ea.chunk.js"
+  },
+  {
+    "revision": "ecbaa8ab10a34f0682e2",
+    "url": "/websitetutorial/static/js/runtime~main.e77f16cd.js"
+  }
+]);
